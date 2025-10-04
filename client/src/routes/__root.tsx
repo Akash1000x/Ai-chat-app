33ba48d87955cbd9f951cc917d4ad25bad82d2c1
akash <kumawatap63@gmail.com>
2025-10-04 22:55:12 +0530

    client: migrated to tanstack router

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,39 @@
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+// import Header from '../components/Header'
+
+import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
+
+import type { QueryClient } from '@tanstack/react-query'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import AppSidebar from '@/components/app-sidebar'
+
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  component: () => (
+    <>
+      {/* <Header /> */}
+      <SidebarProvider>
+        <AppSidebar />
+        <Outlet />
+      </SidebarProvider>
+      <TanStackDevtools
+        config={{
+          position: 'bottom-right',
+        }}
+        plugins={[
+          {
+            name: 'Tanstack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+          TanStackQueryDevtools,
+        ]}
+      />
+    </>
+  ),
+})
---
+++ client/src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+  return (
+    <div className="text-9xl p-4">
+      😎
+    </div>
+  )
+}
