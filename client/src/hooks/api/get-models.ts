a254b226bafcf5e8b30ee65d458d3a563c838c14
akash <kumawatap63@gmail.com>
2025-10-05 18:07:31 +0530

    Added get-models api hook

---
+++ client/src/hooks/api/get-models.ts
@@ -0,0 +1,15 @@
+import { apiClient } from "@/lib/api-client"
+import type { ModelsCategory } from "@/types/models";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchModels = async (): Promise<ModelsCategory[]> => {
+  const { data } = await apiClient.get(`/chat/get-models`)
+  return data.data;
+}
+
+export const useGetModels = () => {
+  return useQuery<ModelsCategory[], Error>({
+    queryKey: ["models"],
+    queryFn: () => fetchModels(),
+  })
+}
