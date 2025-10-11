b269cb81f31862efade26e3d9b895dd02a920a65
akash <kumawatap63@gmail.com>
2025-10-12 00:32:11 +0530

    server: added better-auth utils

---
+++ server/src/utils/auth.ts
@@ -0,0 +1,22 @@
+import { db } from "@/db/index.js";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: false,
+        defaultValue: "user",
+        input: false, // don't allow user to set role
+      },
+    },
+  },
+});
