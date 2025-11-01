2def6bf3cfe1b0156d2d42dc7b99ca329f6ce26a
akash <kumawatap63@gmail.com>
2025-11-01 18:48:42 +0530

    client: use query mutation hook for updates

--- client/src/hooks/api/delete-conversation.ts
+++ client/src/hooks/api/delete-conversation.ts
@@ -1,6 +1,23 @@
 import { apiClient } from "@/lib/api-client"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import type { AxiosError } from "axios"
+import { toast } from "sonner"
 
-export const deleteConversation = async (threadId: string) => {
-  const res = await apiClient.delete(`/v1/chat/delete-conversation?threadId=${threadId}`)
-  return res.data
+export const useDeleteConversationMutation = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async ({ threadId }: { threadId: string }) => {
+      const res = await apiClient.delete(`/v1/chat/delete-conversation?threadId=${threadId}`)
+      return res.data
+    },
+    onSuccess: () => {
+      toast.success("Conversation deleted successfully")
+      queryClient.invalidateQueries({
+        queryKey: ["threads"],
+      })
+    },
+    onError: (error: AxiosError<{ error: { message: string } }>) => {
+      toast.error(error.response?.data?.error?.message || "Failed to delete conversation")
+    },
+  })
 }
--- client/src/hooks/api/get-suggestioin-questions.ts
+++ client/src/hooks/api/get-suggestioin-questions.ts
@@ -3,17 +3,19 @@ import { useQuery } from "@tanstack/react-query";
 
 type Category = "create" | "explore" | "code" | "learn"
 
-type SuggestionData = {
-  [key in Category]: string[]
+type SuggestionQuestion = {
+  id: string
+  category: Category
+  questions: string[]
 }
 
-const fetchSuggestionQuestions = async (): Promise<SuggestionData> => {
+const fetchSuggestionQuestions = async (): Promise<SuggestionQuestion[]> => {
   const { data } = await apiClient.get(`/v1/get-suggestions-questions`)
   return data.data;
 }
 
 export const useGetSuggestionQuestions = () => {
-  return useQuery<SuggestionData, Error>({
+  return useQuery<SuggestionQuestion[], Error>({
     queryKey: ["suggestion-questions"],
     queryFn: () => fetchSuggestionQuestions(),
   })
