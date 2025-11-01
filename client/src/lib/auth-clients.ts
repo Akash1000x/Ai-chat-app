6fe431da9e680cd28d2d61d0ab840c2ea3a322a6
akash <kumawatap63@gmail.com>
2025-11-01 18:49:12 +0530

    client: added role in user types

--- client/src/lib/auth-clients.ts
+++ client/src/lib/auth-clients.ts
@@ -1,5 +1,16 @@
 import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  plugins: [
+    inferAdditionalFields({
+      user: {
+        role: {
+          type: "string",
+          required: false,
+        },
+      },
+    }),
+  ]
 });
