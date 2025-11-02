7053530e955767a75a96fc240c9b91d092ebf8c7
akash <kumawatap63@gmail.com>
2025-11-02 23:30:21 +0530

    client: add useDebounce hook

---
+++ client/src/hooks/use-debounce.ts
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+export default function useDebounce(value: string, delay: number = 500) {
+  const [debouncedValue, setDebouncedValue] = React.useState(value)
+  React.useEffect(() => {
+    const handler = setTimeout(() => setDebouncedValue(value), delay)
+    return () => clearTimeout(handler)
+  }, [value, delay])
+  return debouncedValue
+}
